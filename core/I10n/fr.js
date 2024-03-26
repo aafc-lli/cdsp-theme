d148206528ed904458f451ccd47d25a8e4f18c38
msinclair <michael.sinclair@agr.gc.ca>
2024-03-26 13:58:38 +0000

    add core fr trans

---
+++ core/I10n/fr.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "files_external",
+    {
+        "Living Labs Cloud Data Storage Platform" : "Plateforme de stockage de données infonuagique des laboratoires vivants",
+        "Home" : "Accueil",
+        "Magnify icon":  "Icône de loupe",
+        "Date Modified": "Date modifiée",
+        "Log in" : "Connexion",
+        "Username or\temail" : "Nom d'utilisateur ou\tadresse e-mail",
+        "_Please enter {minSearchLength} character or more to search_::_Please enter {minSearchLength} characters or more to search_" : ["Veuillez saisir {minSearchLength} caractères ou plus pour la recherche","Veuillez saisir {minSearchLength} caractères ou plus pour la recherche","Veuillez saisir {minSearchLength} caractères ou plus pour la recherche"],
+    },
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
