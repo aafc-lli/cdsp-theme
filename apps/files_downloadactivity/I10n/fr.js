72440ca22f9c9148ff338d2934c2319cc876d439
Mike Sinclair <michael.sinclair@agr.gc.ca>
2024-04-02 01:12:54 -0700

    add translations for downloadactivity

---
+++ apps/files_downloadactivity/I10n/fr.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "files_downloadactivity",
+    {
+        "Creates activities for downloads of files that were shared with other users or a group" : "Crée des activités pour le téléchargement de fichiers partagés avec d'autres utilisateurs ou un groupe" 
+    },
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
